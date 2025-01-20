d7349da1ec5b5835165029cb0bbf631cc3a5f859
Amanda Ajredini <159648462+AmandaAjredini@users.noreply.github.com>
2025-01-20 18:06:17 +0000

    Delete head.php

--- head.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <!-- Separate the head on its own page as all pages will have the same head contents -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amanda's Book Haven</title>
-    <link rel="stylesheet" href="style.css">
-</head>
